4c2389a27f32eac1f286a671e89e532b32acf043
Yonier <yonierruiz1234@gmail.com>
2025-11-08 12:37:26 -0500

    CC

---
+++ Tecnico/HTML/JavaScrip/Variables.js
@@ -0,0 +1,14 @@
+let nombre = "Juan";
+let año_nacimiento = 2009;
+let edad = 2025 - año_nacimiento;
+
+console.log("Nombre: " , nombre);
+console.log("Año de nacimiento: " , año_nacimiento);
+console.log("edad: " , edad);
+
+
+if(edad >= 18){
+    console.log("Puede votar");
+}else{
+    console.log("No puede votar");
+}
