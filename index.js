3d5e069f92ec38488cdbf9ae255cb82d5f03a210
diego586-hue <diegoencizo.com@gmail.com>
2026-06-13 11:17:27 -0500

    ccs

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+
+.connect(process.env.MONGO_URI)
+
+.then(()=>{console.log(`mongoose activo`)})
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`servidor activo en : http://localhost:${process.env.PORT}`)
+});
