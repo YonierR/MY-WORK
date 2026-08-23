b71f184e8faa617483f93b8b756045fdf0cbc023
YonierR <yonierruiz1234@gmail.com>
2026-08-22 22:10:13 -0500

    React

--- mi-React/src/App.jsx
+++ mi-React/src/App.jsx
@@ -1,19 +1,28 @@
+import React from 'react'
 import './App.css'
-import perfil from './component/perfil'
+import Perfil from './component/perfil'
 
 function App() {
   const nombre = "luis"
+  const [contador, setContador] = React.useState(0)
   return(
     <main className="contenedor">
       <h1>Hola, {nombre}!</h1>
       <p>mi_React</p>
       <p></p>
-      <perfil
+      <Perfil
       nombre="carlos"
       profesion="Desarrollador"
       imagen="https://name-img.charlies-names.com/name-luis-happy-male.webp"
       />
+      <p></p>
+      <p>{contador}</p>
+      <p></p>
       <button onClick={() => alert('funciona')}>boton</button>
+      <p></p>
+      <button onClick={() => setContador(contador + 1)}>suma</button>
+      <p></p>
+      <button onClick={() => setContador(contador - 1)}>resta</button>
     </main>
   )
 }
